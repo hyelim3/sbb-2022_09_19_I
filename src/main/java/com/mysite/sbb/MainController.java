1e603ead3c0b74be67c2ed755013d1ba7cbaad24
xanqus <sbk98031816@gmail.com>
2022-09-19 17:49:12 +0900

    @ResponseBody 어노테이션

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,13 +2,16 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/sbb")
-    public void index() {
+    @ResponseBody
+    public String index() {
         System.out.println("sbb");
+        return "sbb";
     }
 
 }
