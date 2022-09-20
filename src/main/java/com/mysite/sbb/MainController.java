2b340ac035c55f9639e359e06eb0c5f6af5ff0b5
xanqus <sbk98031816@gmail.com>
2022-09-20 15:32:29 +0900

    @GetMapping, @PostMapping, @RequestParam

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,8 +1,7 @@
 package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -13,5 +12,35 @@ public class MainController {
         System.out.println("sbb");
         return "sbb";
     }
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showPage1() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type='number' name="age" placeholder="나이"/>
+                    <input type="submit" value="page2로 POST방식으로 이동" />
+                </form>
+                """;
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(value = "age", defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>POST방식으로 옴</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(value = "age", defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>GET방식으로 옴</h1>
+                """.formatted(age);
+    }
+
 
 }
