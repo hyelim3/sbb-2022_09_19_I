8b9bf6b474283d0cb421244d5c6210f7087e329b
xanqus <sbk98031816@gmail.com>
2022-09-23 15:21:33 +0900

    jpa findall 메서드

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -27,6 +30,16 @@ class SbbApplicationTests {
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q2);
+	}
+
+	@Test
+	void getAll() {
+		List<Question> all = questionRepository.findAll();
+		assertEquals(8, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요~~~?", q.getSubject());
+
 	}
 
 }
