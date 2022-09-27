4383304801bd79508bf0a53ee6ea09e10faaebde
xanqus <sbk98031816@gmail.com>
2022-09-27 15:40:52 +0900

    의존성 주입

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -1,13 +1,19 @@
 package com.mysite.sbb.question.controller;
 
+import com.mysite.sbb.question.dao.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/question/list")
     public String list() {
+
         return "question_list";
     }
 }
