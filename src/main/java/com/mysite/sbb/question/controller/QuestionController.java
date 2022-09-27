a9a0352db3212e4e5c609a7a6848a92a0fd0ddac
xanqus <sbk98031816@gmail.com>
2022-09-27 15:49:45 +0900

    질문 리스트 템플릿 작업

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -1,9 +1,13 @@
 package com.mysite.sbb.question.controller;
 
 import com.mysite.sbb.question.dao.QuestionRepository;
+import com.mysite.sbb.question.domain.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,8 +16,9 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
 
     @RequestMapping("/question/list")
-    public String list() {
-
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
