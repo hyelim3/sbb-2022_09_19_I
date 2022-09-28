82f2ac53abcc632fe728972386ad5d68b42c4a26
xanqus <sbk98031816@gmail.com>
2022-09-28 16:15:40 +0900

    질문 상세 페이지

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.mysite.sbb.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -28,4 +29,11 @@ public class QuestionController {
         return "question_list";
     }
 
+    @RequestMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable Integer id) {
+        System.out.println("id: " + id);
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
 }
--- src/main/java/com/mysite/sbb/question/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -2,10 +2,12 @@ package com.mysite.sbb.question.service;
 
 import com.mysite.sbb.question.dao.QuestionRepository;
 import com.mysite.sbb.question.domain.Question;
+import com.mysite.sbb.util.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> questionOptional = questionRepository.findById(id);
+        if(questionOptional.isPresent()) {
+            return questionOptional.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
