5ef978b86c46973930bfbe187f0e16587a37dc7e
xanqus <sbk98031816@gmail.com>
2022-09-28 15:17:20 +0900

    서비스 도입

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -1,7 +1,7 @@
 package com.mysite.sbb.question.controller;
 
-import com.mysite.sbb.question.dao.QuestionRepository;
 import com.mysite.sbb.question.domain.Question;
+import com.mysite.sbb.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +13,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
+
+    @RequestMapping("")
+    public String index() {
+        return "redirect:/question/list";
+    }
 
     @RequestMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = questionRepository.findAll();
+
+        List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
+
 }
---
+++ src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb.question.service;
+
+import com.mysite.sbb.question.dao.QuestionRepository;
+import com.mysite.sbb.question.domain.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return questionRepository.findAll();
+    }
+}
