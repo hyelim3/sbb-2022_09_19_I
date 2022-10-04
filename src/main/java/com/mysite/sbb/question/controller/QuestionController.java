9eb1c377d8b103ccad381dcc5c9844e5c601fe0e
xanqus <sbk98031816@gmail.com>
2022-10-04 15:29:21 +0900

    질문 등록 처리

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -5,9 +5,7 @@ import com.mysite.sbb.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +34,14 @@ public class QuestionController {
 
     @GetMapping("/create")
     public String questionCreate() {
-        return "question_from";
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content){
+
+        questionService.create(subject, content);
+
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/mysite/sbb/question/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -6,6 +6,7 @@ import com.mysite.sbb.util.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -19,5 +20,13 @@ public class QuestionService {
 
     public Question getQuestion(Integer id) {
         return questionRepository.findById(id).orElseThrow(() -> new DataNotFoundException("question not found"));
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
     }
 }
